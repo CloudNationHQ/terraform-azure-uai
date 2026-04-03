1959acaf1910311201971a8f7ca128789b120775
dkooll <dennis.kool@cloudnation.nl>
2026-04-03 09:00:14 +0200

    feat: add missing outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "config" {
   description = "Contains all user assigned identity configuration"
   value       = azurerm_user_assigned_identity.uai
 }
+
+output "federated_credentials" {
+  description = "Contains all federated identity credential configurations"
+  value       = azurerm_federated_identity_credential.creds
+}
